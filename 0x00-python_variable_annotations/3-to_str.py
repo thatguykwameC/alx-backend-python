20a324bb1e7b252adf1633fe2623d2ff5f87de4b
thatguykwameC <thatguykwamec@gmail.com>
2024-08-01 05:57:29 -0400

    A function that returns a string rep

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Returns the string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n: A float.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)

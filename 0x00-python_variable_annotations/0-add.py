5fcf00abfe38de7c23cf99b1f6e44de8915d8f2d
thatguykwameC <thatguykwamec@gmail.com>
2024-08-01 05:39:52 -0400

    A module that sums two floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""This function adds two floats together."""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats together.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of both floats.
+    """
+    return a + b

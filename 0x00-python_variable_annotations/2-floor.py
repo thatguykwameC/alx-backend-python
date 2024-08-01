0db53047be76fe6878b1975d07541e70f7e3c511
thatguykwameC <thatguykwamec@gmail.com>
2024-08-01 05:51:54 -0400

    A function that returns a rounded integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""This function returns the rounded down integer of a float."""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the rounded down integer of a float.
+
+    Args:
+        n (float): The float to be rounded down.
+
+    Returns:
+        int: The rounded down integer of the float.
+    """
+    return int(n)
